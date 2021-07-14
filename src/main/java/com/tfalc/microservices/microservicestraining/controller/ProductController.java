ec337ef27ea0d2f23cc5322adc3350db0b26a605
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2021-07-13 23:14:35 -0300

    Create ProductController.java

---
+++ src/main/java/com/tfalc/microservices/microservicestraining/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.tfalc.microservices.microservicestraining.controller;
+
+import com.tfalc.microservices.microservicestraining.model.Product;
+import com.tfalc.microservices.microservicestraining.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/product")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @RequestMapping(method = RequestMethod.POST)
+    Product create(@RequestBody Product product){
+        return productRepository.save(product);
+    }
+}

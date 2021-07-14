1e166fdce97467526bc091862ca0945f801e8838
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2021-07-13 23:04:37 -0300

    Create Product.java

---
+++ src/main/java/com/tfalc/microservices/microservicestraining/model/Product.java
@@ -0,0 +1,34 @@
+package com.tfalc.microservices.microservicestraining.model;
+
+public class Product {
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    private Long id;
+    private String name;
+    private Integer amount;
+
+
+}

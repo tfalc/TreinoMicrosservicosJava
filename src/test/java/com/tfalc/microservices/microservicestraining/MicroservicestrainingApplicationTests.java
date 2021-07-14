99e63dec64e4022405f210bfb8500f4013cc6f4b
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2021-07-13 22:47:58 -0300

    Initial commit

---
+++ src/test/java/com/tfalc/microservices/microservicestraining/MicroservicestrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfalc.microservices.microservicestraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicestrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
